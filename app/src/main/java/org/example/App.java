f88c5cfd0707221f4af49018af4defeb96483951
zliu2252-jim <zliu2252@wisc.edu>
2025-06-15 18:58:27 -0500

    Initial commit

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class App {
+  public int getInput() {
+    // Implement your function here
+  }
+
+  public static void main(String[] args) {
+    // Test your function here in a driver program
+  }
+}
---
+++ app/src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+  // We have not learned how to test a while loop with unit tests.
+  // It is possible to do, but you need to handle the user input and validation.
+  // Thus, for this lab, you do NOT need to write unit tests.
+  // Instead, test your code with a driver function
+}
